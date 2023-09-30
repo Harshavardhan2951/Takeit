5372f9784ec06d4f8280a439a6a67ccbf28794b0
= <=>
2023-09-30 17:27:33 +0530

    JavaScript Practices

---
+++ JAVASCRIPT_BRO-CODE/Arithmatic Expression/index.js
@@ -0,0 +1,16 @@
+/*Arithmatic expression is a combination of Value, Variables....
+    Operands(values, varibles, etc..)
+    operators(+, - ,/, *)
+    that can be evaluated to a value
+    eg: y = x+5;    
+*/
+let student = 30;
+
+
+/*Modulus operator gives the remainder as a solution [%] */
+
+let extraStudents = 9;
+console.log(student % extraStudents)
+
+/*Exponentiation Operator [**] */
+console.log(student ** 3)
---
+++ JAVASCRIPT_BRO-CODE/Variables/variable.js
@@ -0,0 +1,17 @@
+/* 
+    A Variable is a container for storing data 
+    A Variable behaves as if it was the value that it contains two steps
+
+    //1.Declaration : (var, let, const)
+    //2.Assignment : (= assignment operator)
+*/
+let firstName = "Monkey D ";
+let age = 7;
+let student = true;
+console.log(age);
+console.log(firstName , "His age is " , age);
+console.log(student);
+
+document.getElementById("p1").innerHTML = "Hello ", firstName, "Luffy";
+document.getElementById("p2").innerHTML = "You'er Age is ", age, " years old";
+document.getElementById("p3").innerHTML = "So u're a Student is it True!!!! "+ student
